f2ce048b3162a2b0b398ee7d38dfd526eacd4c54
kitcat-law <saltispepper1@gmail.com>
2025-10-21 18:19:13 +0100

    Update DraggableWidget.java

--- runtime/src/main/java/me/shedaniel/rei/impl/client/gui/widget/DraggableWidget.java
+++ runtime/src/main/java/me/shedaniel/rei/impl/client/gui/widget/DraggableWidget.java
@@ -30,6 +30,9 @@ import me.shedaniel.math.impl.PointHelper;
 import me.shedaniel.rei.api.client.gui.widgets.WidgetWithBounds;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.events.GuiEventListener;
+import net.minecraft.client.input.MouseButtonEvent;
+import net.minecraft.client.input.KeyEvent;
+import net.minecraft.client.input.CharacterEvent;
 import org.jetbrains.annotations.ApiStatus;
 
 @ApiStatus.Internal
@@ -60,7 +63,7 @@ public abstract class DraggableWidget extends WidgetWithBounds {
     }
     
     @Override
-    public boolean mouseDragged(double double_1, double double_2, int int_1, double double_3, double double_4) {
+    public boolean mouseDragged(MouseButtonEvent event, double double_3, double double_4) {
         Point mouse = PointHelper.ofMouse();
         if (int_1 == 0) {
             if (!dragged) {
@@ -78,7 +81,7 @@ public abstract class DraggableWidget extends WidgetWithBounds {
             return true;
         }
         for (GuiEventListener listener : children())
-            if (listener.mouseDragged(double_1, double_2, int_1, double_3, double_4))
+            if (listener.mouseDragged(event, double_3, double_4))
                 return true;
         return false;
     }
@@ -86,7 +89,7 @@ public abstract class DraggableWidget extends WidgetWithBounds {
     public abstract Point processMidPoint(Point midPoint, Point mouse, Point startPoint, Window window, int relateX, int relateY);
     
     @Override
-    public boolean mouseReleased(double double_1, double double_2, int int_1) {
+    public boolean mouseReleased(MouseButtonEvent event) {
         if (int_1 == 0)
             if (dragged) {
                 dragged = false;
@@ -94,7 +97,7 @@ public abstract class DraggableWidget extends WidgetWithBounds {
                 return true;
             }
         for (GuiEventListener listener : children())
-            if (listener.mouseReleased(double_1, double_2, int_1))
+            if (listener.mouseReleased(event))
                 return true;
         return false;
     }
